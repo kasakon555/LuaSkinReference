f8e2b0643ff7acb5b99e7aff987d01fe0a753bac
kasakon555 <kasakon555@gmail.com>
2020-10-08 20:02:30 +0900

    文字列の場合を追加

--- basic/01_dstimage/main.lua
+++ basic/01_dstimage/main.lua
@@ -23,18 +23,25 @@ local function main()
 	-- ここにスキンを記述していく
 	-- 画像ファイルの登録
     skin.source = {
+		-- img/image01.pngを1というIDで登録
 		{id = 1, path = "img/image01.png"},
+		-- idには文字列も可能
+		{id = "sample", path = "img/image01.png"},
 	}
 	-- 画像の切り出し
 	skin.image = {
 		-- orangeという名前でidが1の画像のx,y軸が0，0の地点から横300px,縦300pxを切り出す
 		{id = "orange", src = 1, x = 0, y = 0, w = 300, h = 300},
+		{id = "blue", src = "sample", x = 0, y = 700, w = 300, h = 300},
 	}
 	-- 画像を配置する
 	skin.destination = {
 		-- orangeという名前で切り出した画像を左下から0px,0pxの位置に横幅300px,高さ300pxの大きさで貼り付ける
 		{id = "orange", dst = {
 			{x = 0, y = 0, w = 300, h = 300}
+		}},
+		{id = "blue", dst = {
+			{x = 1620, y = 0, w = 300, h = 300}
 		}}
 	}
 	return skin
